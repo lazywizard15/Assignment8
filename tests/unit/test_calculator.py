8055a57ad6ab37d8b68b168c343fb4d4f0f3012e
lazywizard15 <karthikben8@gmail.com>
2025-10-28 22:03:43 -0400

    changed testing sectiion

--- tests/unit/test_calculator.py
+++ tests/unit/test_calculator.py
@@ -2,7 +2,7 @@
 
 import pytest  # Import the pytest framework for writing and running tests
 from typing import Union  # Import Union for type hinting multiple possible types
-from app.operations import add, subtract, multiply, divide  # Import the calculator functions from the operations module
+from app.operations import add, subtract, multiply, divide, power, modulo  # Import the calculator functions from the operations module
 
 # Define a type alias for numbers that can be either int or float
 Number = Union[int, float]
